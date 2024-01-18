b2cc090ae9715a4981a17512198c391ff8b4d758
Chessman2003 <145466841+Chessman2003@users.noreply.github.com>
2024-01-18 05:38:32 +0300

    fix BoredActivityPage

--- src/Components/BoredActivityPage/BoredActivityPage.tsx
+++ src/Components/BoredActivityPage/BoredActivityPage.tsx
@@ -1,42 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { PartBoredActivityType } from '../lib/types/boredActivityType';
 import { getPartBoredActivity } from '../lib/getPartBoredActivity';
-import Button from '../Button/Button';
-import { DataDisplay } from '../DataDisplay/DataDisplay';
-import Loader from '../Loader/Loader';
+
+
 
 
 type Props = {
     url: string
 };
 
-// ToDo: Именновать в соответствии с решаемыми задачами !!!!!!!!!!!!!!!!!!!!!!
+
 export const BoredActivityPage = ({ url }: Props) => {
-    const [activityItem, setActivityItem] = useState<PartBoredActivityType>({ // ToDo: получение в объект части данных поля (activity и key)
+    const [activityItem, setActivityItem] = useState<PartBoredActivityType>({ 
         activity: "",
         key: ""
     })
 
-    const [loading, setLoading] = useState<Boolean>(true)
+    
     const loadNextObject = async () => {
         const item = await getPartBoredActivity(url);
-
         setActivityItem(item);
-        setLoading(false);
     };
 
     useEffect(() => {
         loadNextObject()
     }, [url])
 
-    // ToDo: Переделать через несколько компонентов, панель управления + компонент отображения данных
+   
     return (
-        <div className="NewApiWrapper">
-            <Button onClick={loadNextObject} text={'Следующая активность'} />
-            {loading ?
-                (<Loader text={'Загрузка...'} />) :
-                (<DataDisplay first={activityItem.activity} second={activityItem.key} />)
-            }
-        </div>
+                    <div>{activityItem.activity}</div>
+                    <div>{activityItem.key}</div>
     );
 }
